5e9612a0b82254adba21415cb0a995a235107629
Andrés Giraldo <andresgircar@gmail.com>
2024-04-17 10:52:06 -0500

    Adjusting content and menu options

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const hamburgerMenu = document.querySelector('.hamburger-menu');
     const mobileMenu = document.querySelector('.mobile-menu');
     const closeIcon = document.querySelector('.close-icon');
+    const scrollLinks = document.querySelectorAll('.scroll-link');
 
     satelite.addEventListener('click', function() {
         satelite.classList.toggle('satelite-rotation')
@@ -15,8 +16,35 @@ document.addEventListener('DOMContentLoaded', function() {
     })
 
     closeIcon.addEventListener('click', function() {
-        mobileMenu.classList.toggle('hidden');
+        mobileMenu.classList.add('hidden');
     })
+
+    // Itera sobre cada enlace y agrega un event listener para el clic
+    scrollLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            // Evita el comportamiento predeterminado del enlace
+            event.preventDefault();
+
+            // Obtiene el valor del atributo href del enlace
+            const targetId = this.getAttribute('href').substring(1);
+
+            // Obtiene el elemento de destino usando el ID
+            const targetElement = document.getElementById(targetId);
+
+            // Desplázate suavemente al elemento de destino
+            targetElement.scrollIntoView({
+                behavior: 'smooth'
+            });
+
+            const checkIfScrollDone = setInterval(function() {
+                if (Math.abs(targetElement.getBoundingClientRect().top) < 1) {
+                    clearInterval(checkIfScrollDone);
+                    mobileMenu.classList.add('hidden');
+                }
+            }, 100);
+            // Cierre del menú
+        });
+    });
 });
 
 //cursor follow
